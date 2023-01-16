31978d3f2f64381c3a58b2941bdbb98da0ae1321
Sam-0517 <samadhankorde2000@gmail.com>
2023-01-16 22:59:37 +0530

    Added: Welcome message code for Mood Analyzer program

---
+++ src/main/java/com/bridgelabz/mood_analyzer/MoodAnalyzer.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.mood_analyzer;
+
+import java.sql.SQLOutput;
+
+public class MoodAnalyzer {
+
+    public static void main(String[] args) {
+        System.out.println("WELCOME TO MOOD ANALYZER PROGRAM");
+    }
+}
